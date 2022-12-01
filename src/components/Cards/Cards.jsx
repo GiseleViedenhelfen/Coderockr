a94d220fed3f735b2b8fed267d1bdb63dd23b842
Gisele <gisele.viedenhelfen@gmail.com>
2022-12-01 20:15:01 -0300

    correção para o redirecionamento ser pelo clique em qualquer parte do card e não apenas em um botão

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,25 +1,20 @@
 import React, { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import GlobalContext from '../../context/context';
 
 function Cards() {
   const { dataAPI } = useContext(GlobalContext);
-  const navigate = useNavigate();
   return (
     <div>
       {dataAPI && dataAPI.map((item) => (
-        <div key={item.id}>
-          <img src={item.image} alt={item.title} height="100px" />
-          <p>{item.author.name}</p>
-          <h2>{item.title}</h2>
-          <p>{item.content}</p>
-          <button
-            type="button"
-            onClick={() => navigate(`post/${item.id}`)}
-          >
-            mais informações
-          </button>
-        </div>
+        <Link key={item.id} to={`post/${item.id}`}>
+          <div>
+            <img src={item.image} alt={item.title} height="100px" />
+            <p>{item.author.name}</p>
+            <h2>{item.title}</h2>
+            <p>{item.content}</p>
+          </div>
+        </Link>
       ))}
     </div>
   );
