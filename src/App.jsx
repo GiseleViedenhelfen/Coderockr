882c42cae155c6bef39e3dd819aacd07591c6a37
Gisele <gisele.viedenhelfen@gmail.com>
2022-12-01 17:10:27 -0300

    adiciona lint para padronização do código e apaga imagens padrões que vem com o react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <p>
+        Edit
+        <code>src/App.js</code>
+        and save to reload.
+      </p>
+
+    </div>
+  );
+}
+
+export default App;
