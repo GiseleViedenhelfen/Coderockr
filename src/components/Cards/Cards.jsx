0d7029016459e279cd408d7d79b5049f4d390ccd
Gisele <gisele.viedenhelfen@gmail.com>
2022-12-02 23:04:37 -0300

    tranalha a renderização automática de posts e o header fixo

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,28 +1,57 @@
-import React, { useContext } from 'react';
+import React, {
+  useContext, useEffect, useState,
+} from 'react';
 import { Link } from 'react-router-dom';
 import GlobalContext from '../../context/context';
 import './Style.css';
 
 function Cards() {
   const { dataAPI } = useContext(GlobalContext);
+  const [cardsOnload, setCardsOnload] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  useEffect(() => {
+    if (dataAPI.length > 0) {
+      const totalPages = Math.ceil(dataAPI.length / 3);
+      if (currentPage === 2) {
+        setCardsOnload(dataAPI.slice(0, 3));
+      }
+      if (currentPage <= totalPages && cardsOnload.length > 0) {
+        const postsOnView = cardsOnload.length;
+        const nextCardsToLoad = dataAPI.slice(postsOnView, postsOnView + 3);
+        setCardsOnload([...cardsOnload, ...nextCardsToLoad]);
+      }
+    }
+  }, [dataAPI, currentPage]);
+  useEffect(() => {
+    const intersectionObserver = new IntersectionObserver((entries) => {
+      if (entries.some((entry) => entry.isIntersecting)) {
+        setCurrentPage((currentPageInsideState) => currentPageInsideState + 1);
+      }
+    });
+    intersectionObserver.observe(document.querySelector('#scroll'));
+    return () => intersectionObserver.disconnect();
+  }, []);
   return (
     <div className="card-container">
-      {dataAPI && dataAPI.map((item) => (
+      {dataAPI.length > 0 && cardsOnload?.map((item) => (
         <Link
           key={item.id}
           to={`post/${item.id}`}
           className="card-link"
         >
           <div className="card-content">
+
             <img src={item.image} alt={item.title} />
             <div className="card-text-content">
               <p className="card-author-name">{item.author.name}</p>
               <h2 className="card-title">{item.title}</h2>
               <p className="card-p-content">{item.content}</p>
             </div>
+
           </div>
         </Link>
       ))}
+      <span id="scroll" />
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,51 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Style.css';
 import Popup from 'reactjs-popup';
 import ContactForm from '../Contact/Contact';
 
 function Header() {
+  const navigate = useNavigate();
+  const getHeader = document.getElementById('header-container');
+  window.onscroll = () => {
+    const sticky = getHeader.offsetTop;
+    if (window.pageYOffset > sticky) {
+      getHeader.classList.add('sticky');
+    } else {
+      getHeader.classList.remove('sticky');
+    }
+  };
+  const clickHome = () => {
+    const url = document.location.href;
+    if (url === 'http://localhost:3000/') {
+      return document.location.reload(true);
+    }
+    return navigate('/');
+  };
   return (
-    <section className="header-container">
-      <h2>Rocker Blog</h2>
+    <section className="header-container" id="header-container">
+      <button
+        type="button"
+        onClick={clickHome}
+        className="button-homepage"
+      >
+        <h2>Rockr Blog</h2>
+      </button>
+
       <section className="header-subcontainer">
-        <h2>Posts</h2>
+        <button
+          type="button"
+          className="button-header"
+          onClick={() => document.location.reload(true)}
+        >
+          Posts
+        </button>
         <Popup
           trigger={(
-            <button type="button" className="button-contact">
+            <button
+              type="button"
+              className="button-header"
+            >
               Contact
             </button>
           )}
