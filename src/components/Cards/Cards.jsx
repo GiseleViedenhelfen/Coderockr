44c8a607ee721df3858690cf106f650e8a42e7ca
Gisele <gisele.viedenhelfen@gmail.com>
2022-12-05 17:50:04 -0300

    envia a nova lógica para a develop

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,8 +1,9 @@
 import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import GlobalContext from '../../context/context';
-import './Style.css';
-// import seta from '../../icons/Vector.png';
+// import './Style.css';
+import './teste.css';
+import seta from '../../icons/Vector.png';
 
 function Cards() {
   const { dataAPI } = useContext(GlobalContext);
@@ -12,13 +13,14 @@ function Cards() {
     let result;
     if (dataAPI.length > 0) {
       const container = dataAPI.slice(...number);
-      const mapContainer = container.map((card, index) => (
-        <Link to={`/post/${card.id}`} className={`card${index} ${card.id}`}>
-          <img src={card.image} alt={card.title} width="20%" />
+      const mapContainer = container.map((card) => (
+        <Link to={`/post/${card.id}`} className={` ${card.id} link${card.id}`}>
+          <img src={card.image} alt={card.title} />
           <div className="card-text-container">
             <p>{card.author.name}</p>
             <h2>{card.title}</h2>
-            <p>{card.content}</p>
+            <p className="card-p-content">{card.content}</p>
+            <img src={seta} alt="img-seta" />
           </div>
         </Link>
       ));
@@ -28,37 +30,52 @@ function Cards() {
   };
   const renderCardsByScroll = (arr) => {
     const getMainContainerDiv = document.querySelector('.cards-container');
+    const groupContainer = document.createElement('div');
+    groupContainer.classList.add('bloco');
     if (dataAPI.length > 0) {
+      const arrLink = [1, 2, 3, 4, 5, 6];
       const container = [...dataAPI.slice(...arr)];
-      container.forEach(
-        (card, index) => {
+      const getCards = container.map(
+        (card) => {
           // criação das tags
           const cardLink = document.createElement('a');
           const textCardContainer = document.createElement('div');
+          // const arrowContainer = document.createElement('div');
           const authorName = document.createElement('p');
           const content = document.createElement('p');
           const title = document.createElement('h2');
           const img = document.createElement('img');
+          const arrow = document.createElement('img');
           // definição do conteudo de cada tag
           img.src = `${card.image}`;
           img.alt = `${card.title}`;
+          arrow.src = seta;
+          arrow.alt = 'img-seta';
           title.innerHTML = `${card.title}`;
           content.innerHTML = `${card.content}`;
           authorName.innerHTML = `${card.author.name}`;
           cardLink.href = `/post/${card.id}`;
           // atribuição de classes
           textCardContainer.classList.add('card-text-container');
-          cardLink.classList.add(`card${index}`);
           cardLink.classList.add(`${card.id}`);
+          cardLink.classList.add(`link${arrLink[0]}`);
+          content.classList.add('card-p-content');
+          arrLink.shift();
           // filiação
           textCardContainer.appendChild(authorName);
           textCardContainer.appendChild(title);
           textCardContainer.appendChild(content);
+          textCardContainer.appendChild(arrow);
           cardLink.appendChild(img);
           cardLink.appendChild(textCardContainer);
-          getMainContainerDiv.appendChild(cardLink);
+          groupContainer.appendChild(cardLink);
+          return cardLink;
         },
       );
+      // cria blocos de cards
+      groupContainer.appendChild(...getCards);
+      // adiciona cada bloco por scroll
+      getMainContainerDiv.appendChild(groupContainer);
     }
   };
   const helpScroll = (validation) => {
@@ -72,7 +89,7 @@ function Cards() {
 
   const timeToScroll = () => {
     if (dataAPI.length > 0) {
-      const lastCard = document.getElementsByClassName(pointer[1])[0]?.getBoundingClientRect().top;
+      const lastCard = document.getElementsByClassName(`${pointer[1]}`)[0]?.getBoundingClientRect().top;
       const screenSize = window.innerHeight;
       const getResult = lastCard < screenSize;
       helpScroll(getResult);
@@ -81,7 +98,9 @@ function Cards() {
   return (
     <div className="cards-container">
       {window.addEventListener('scroll', timeToScroll)}
-      {FirstCardsToShow([0, 6])}
+      <div className="bloco">
+        {FirstCardsToShow([0, 6])}
+      </div>
     </div>
   );
 }
