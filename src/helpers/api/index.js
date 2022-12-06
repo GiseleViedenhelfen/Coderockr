e6460b2c8baab14e9490bcbe88f15729e7466fef
Gisele <gisele.viedenhelfen@gmail.com>
2022-12-05 23:05:53 -0300

    adiciona readme

--- src/helpers/api/index.js
+++ src/helpers/api/index.js
@@ -1,13 +1,12 @@
-async function requestApi() {
+export async function requestApi() {
   const url = 'https://sample-posts.coderockr.com/api/posts/';
   const data = await fetch(url);
   const response = await data.json();
   return response;
 }
-async function requestApiById(id) {
+export async function requestApiById(id) {
   const url = `https://sample-posts.coderockr.com/api/posts/${id}`;
   const data = await fetch(url);
   const response = await data.json();
   return response;
 }
-module.exports = { requestApi, requestApiById };
